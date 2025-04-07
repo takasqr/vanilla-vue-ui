c2055eee19810933745550348bbc95da9dcbd861
takasqr <95556025+takasqr@users.noreply.github.com>
2025-04-08 05:28:36 +0900

    feat: コンポーネント追加

---
+++ src/template/page-block/WPageBlock.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <w-container class="py-8 md:py-10">
+      <slot />
+    </w-container>
+  </div>
+</template>
+
+<script setup lang="ts">
+import WContainer from '../../basic/container/WContainer.vue';
+</script>
---
+++ src/template/section/WSection.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="my-10 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+    <slot name="title" />
+  </div>
+
+  <slot />
+</template>
